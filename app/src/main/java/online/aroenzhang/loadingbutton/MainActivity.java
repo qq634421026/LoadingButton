b6cb3454bdc34597b2bcd204c90b0545bdb7b672
张洪宇 <aroenzhang@sina.com>
2017-11-24 10:28:09 +0800

    提交locala

--- app/src/main/java/online/aroenzhang/loadingbutton/MainActivity.java
+++ app/src/main/java/online/aroenzhang/loadingbutton/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    //测试哦
+    //测试哦是是是
     loadingbutton = (LoadButton) findViewById(R.id.loadingbutton);
   }
 
