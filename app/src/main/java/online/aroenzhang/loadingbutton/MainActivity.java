3846de35ac4f969dec8d78308063ea9addd694de
张洪宇 <aroenzhang@sina.com>
2017-11-24 11:19:09 +0800

    提交localaa

--- app/src/main/java/online/aroenzhang/loadingbutton/MainActivity.java
+++ app/src/main/java/online/aroenzhang/loadingbutton/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    //测试哦是是是sdsdd
     loadingbutton = (LoadButton) findViewById(R.id.loadingbutton);
   }
 
